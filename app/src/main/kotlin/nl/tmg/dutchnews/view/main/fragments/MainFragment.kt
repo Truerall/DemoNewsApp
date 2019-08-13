d4d84b6ebca9deadfa75884382b4bfd9eab7c161
OleksiiKriukov <krukoff.09@gmail.com>
2019-08-13 18:12:58 +0200

    [Common][MainFragment] Architecture components for updating view with data. Basic error handling for MainFragment.

--- app/src/main/kotlin/nl/tmg/dutchnews/view/main/fragments/MainFragment.kt
+++ app/src/main/kotlin/nl/tmg/dutchnews/view/main/fragments/MainFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
 import nl.tmg.dutchnews.R
+import nl.tmg.dutchnews.model.data_models.Article
 import nl.tmg.dutchnews.view.BaseVMFragment
 import nl.tmg.dutchnews.view_model.BaseViewModel
 import nl.tmg.dutchnews.view_model.main.MainViewModel
@@ -18,12 +20,24 @@ class MainFragment : BaseVMFragment<MainViewModel>() {
         fun newInstance() = MainFragment()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.getTopHeaders()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        viewModel.errorEventLiveData.observeEvent(viewLifecycleOwner, this::handleError)
+        viewModel.topHeadersLiveData.observeNonNull(viewLifecycleOwner, this::displayData)
         return inflater.inflate(R.layout.frg_main, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.getTopHeaders()
+    private fun displayData(issuesList: List<Article>) {
+        //adapter.setData(issuesList)
+    }
+
+    private fun handleError(errorMessage: String) {
+        view?.let {
+            Snackbar.make(it, errorMessage, Snackbar.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/kotlin/nl/tmg/dutchnews/view_model/main/MainViewModel.kt
+++ app/src/main/kotlin/nl/tmg/dutchnews/view_model/main/MainViewModel.kt
@@ -3,27 +3,37 @@ package nl.tmg.dutchnews.view_model.main
 import android.util.Log
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
+import nl.tmg.dutchnews.model.data_models.Article
 import nl.tmg.dutchnews.model.repository.INewsRepository
+import nl.tmg.dutchnews.utils.architecture.Event
+import nl.tmg.dutchnews.utils.architecture.EventLiveData
+import nl.tmg.dutchnews.utils.architecture.NonNullMutableLiveData
 import nl.tmg.dutchnews.view_model.BaseViewModel
 import nl.tmg.dutchnews.view_model.IBaseViewModel
 import javax.inject.Inject
 
 interface IMainViewModelContract : IBaseViewModel {
+    val errorEventLiveData: EventLiveData<String>
+    val topHeadersLiveData: NonNullMutableLiveData<List<Article>>
     fun getTopHeaders()
 }
 
 class MainViewModel @Inject constructor(private val newsRepository: INewsRepository) : BaseViewModel(),
     IMainViewModelContract {
 
+    override val errorEventLiveData = EventLiveData<String>()
+    override val topHeadersLiveData = NonNullMutableLiveData<List<Article>>()
+
+
     override fun getTopHeaders() {
         compositeDisposable += newsRepository.getTopHeadlines()
             .subscribeBy(
                 onSuccess = {
-                    Log.d("DutchNews", "some data > ${it[0].description}")
-                    Log.d("DutchNews", "some data > ${it[0].publishedAt}")
+                    topHeadersLiveData.value = it
                 },
                 onError = {
                     Log.d("DutchNews", "WOW! thats error. check logs")
+                    errorEventLiveData.value = Event(it.message ?: "Unexpected error")
                 }
             )
     }
